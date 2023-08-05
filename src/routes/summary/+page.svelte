f812ea13d47eb12a90c149f818c55523cf70d9f1
German Burgardt <german@getgloby.com>
2023-08-05 03:08:12 -0300

    added convert text to html gpt service

--- src/routes/summary/+page.svelte
+++ src/routes/summary/+page.svelte
@@ -1,42 +1,130 @@
-
-<!-- src/routes/summary.svelte -->
 <script>
-import { summaryStore } from '../../summaryStore.js';
+    import { summaryStore } from '../../summaryStore.js';
+    import { onMount } from 'svelte';
 
-let summary = {
-    transcriptions: [],
-    summaries: [],
-    highlights: "",
-};
+    let summary = {
+        transcriptions: [],
+        summaries: [],
+        highlights: "",
+    };
 
-summaryStore.subscribe(data => {
-    console.log("Data from store: ", data   );
-    summary = data;
-});
+    summaryStore.subscribe(data => {
+        console.log("Data from store: ", data);
+        summary = data;
+    });
+
+    async function convertResume(text) {
+        const apiUrl = 'http://localhost:5001/api/convert-text';
+        const payload = { text };
+        try {
+            const response = await fetch(apiUrl, {
+                method: 'POST',
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify(payload)
+            });
+            if (!response.ok) {
+                throw new Error("Error en la respuesta del servidor");
+            }
+            const data = await response.json();
+            return data;
+        } catch (error) {
+            console.error(error);
+        }
+    }
+
+    onMount(async () => {
+        for (let i = 0; i < summary.summaries.length; i++) {
+            const htmlText = await convertResume(summary.summaries[i].summary);
+            summary.summaries[i].summary = htmlText;
+        }
+    });
+
+    let currentTab = 'summary';
 </script>
 
 <main class="container">
-    <!-- <div class="container">
-        <h1>Análisis del video</h1>
-        <h2>Resumen</h2>
-        <p>El video plantea reflexiones acerca del cambio constante de partidos políticos en Estados Unidos en contraste con la estabilidad del Partido Comunista en China. Sin embargo, se señala que, a pesar de estos cambios en EE. UU., la política pública no sufre modificaciones significativas.</p>
-        <h2>Comparación con China</h2>
-        <p>En China, aunque el Partido Comunista se mantiene en el poder, la política pública experimenta cambios revolucionarios. A pesar de la apertura de China al mercado internacional, el Partido Comunista no se deja dominar por los capitalistas.</p>
-        <h2>Situación en Estados Unidos</h2>
-        <p>En contraposición, en Estados Unidos se sugiere que los oligarcas y capitalistas ejercen un gran control sobre la política. Destacando la influencia de los grupos de interés y la legalización del lobby.</p>
-        <h2>Conclusiones</h2>
-        <p>El análisis final indica que en China, aunque existe un mercado próspero, el poder del capital no puede sobreponerse al poder político del Partido Comunista. En comparación con Estados Unidos, las empresas y magnates en China pueden ser restringidos por el partido si no representan los intereses del proletariado.</p>
-        <div class="row">
-            <a class="button button-primary" href="/analisis-anterior">Ver análisis anterior</a>
-            <a class="button button-primary" href="/nuevo-analisis">Hacer otro análisis</a>
+    <h1>Aquí está tu resumen</h1>
+    <div class="tabs">
+        <button on:click={() => currentTab = 'summary'}>Resumen</button>
+        <button on:click={() => currentTab = 'transcriptions'}>Transcripciones</button>
+        <button on:click={() => currentTab = 'highlights'}>Highlights</button>
+    </div>
+    {#if currentTab === 'summary'}
+        <div id="summary">
+            <h2>Resumen:</h2>
+            <ul>
+            {#each summary.summaries as item (item.segment)}
+                <li>{@html item.summary}</li>
+            {/each}
+            </ul>
         </div>
-    </div> -->
-    
+    {:else if currentTab === 'transcriptions'}
+        <div id="transcriptions">
+            <h2>Transcripciones:</h2>
+            <ul>
+            {#each summary.transcriptions as transcript, index (index)}
+                <li>{transcript}</li>
+            {/each}
+            </ul>
+        </div>
+    {:else if currentTab === 'highlights'}
+        <div id="highlights">
+            <h2>Highlights:</h2>
+            <p>{summary.highlights}</p>
+        </div>
+    {/if}
+    <a class="button button-primary" href="/input">Hacer otro resumen</a>
+</main>
+
+<!-- <script>
+    import { summaryStore } from '../../summaryStore.js';
+    import { onMount } from 'svelte';
+
+    let summary = {
+        transcriptions: [],
+        summaries: [],
+        highlights: "",
+    };
+
+    summaryStore.subscribe(data => {
+        console.log("Data from store: ", data);
+        summary = data;
+    });
+
+    async function convertTextToHTML(text) {
+        // const apiUrl = 'http://ec2-54-158-67-18.compute-1.amazonaws.com:5001/api/convert-text';
+        const apiUrl = 'http://localhost:5001/api/convert-text';
+        const payload = { text };
+        try {
+            const response = await fetch(apiUrl, {
+                method: 'POST',
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify(payload)
+            });
+            if (!response.ok) {
+                throw new Error("Error en la respuesta del servidor");
+            }
+            const data = await response.json();
+            return data;
+        } catch (error) {
+            console.error(error);
+        }
+    }
+
+    onMount(async () => {
+        for (let i = 0; i < summary.summaries.length; i++) {
+            const htmlText = await convertTextToHTML(summary.summaries[i].summary);
+            summary.summaries[i].summary = htmlText;
+        }
+    });
+</script>
+
+<main class="container">
     <h1>Aquí está tu resumen</h1>
     <h2>Resumen:</h2>
     <ul>
     {#each summary.summaries as item (item.segment)}
-        <li>Segmento {item.segment}: {item.summary}</li>
+        <li>{@html item.summary}</li>
     {/each}
     </ul>
     <h2>Transcripciones:</h2>
@@ -49,10 +137,4 @@ summaryStore.subscribe(data => {
     <p>{summary.highlights}</p>
     <a class="button button-primary" href="/input">Hacer otro resumen</a>
 </main>
-  
-<!-- 
-<main class="container">
-    <h1>Aquí está tu resumen</h1>
-    <p>Resumen del video...</p> 
-    <a class="button button-primary" href="/input">Hacer otro resumen</a>
-</main> -->
+     -->
